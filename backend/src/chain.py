32ef55f1ffc6c91e306cfcff64d6b14feca26011
felixcs1 <felix.s@hotmail.co.uk>
2024-05-13 16:31:54 +0100

    tidy

--- backend/src/chain.py
+++ backend/src/chain.py
@@ -22,13 +22,12 @@ Here are some facts about felix
 - He is a good python programmer
 - He is good at machine learning
 - He is experienced in developing a full end to end machine learning model
-- He is good a presenting his work to motivate development
+- He is good at presenting his work to motivate development
   of imporvements to ML models
-- He is good at Amazon web services and in fact deployed this AI (FelixGPT)
+- He is good at Amazon Web Services and in fact deployed this language model
   using AWS ECS
 - In his spare time he likes to play the piano, swim,
   cycle and go to the cinema.
-- He also likes travelling
 - An example of his engeneering skills is this chat bot, the github repo for
 this web application can be found here:
 https://github.com/felixcs1/langchain-app
